1a139cd0d7ba1ae9c272a19a1f49f04ee52db737
SanjayVelu <51081868+SanjayVelu@users.noreply.github.com>
2019-08-04 21:43:48 +0530

    Create prefix.py

---
+++ prefix.py
@@ -0,0 +1,21 @@
+def commonprefixutil(str1,str2):
+    n1=len(str1)
+    n2=len(str2)
+    result=""
+    j=0
+    i=0
+    while(i <= n1 -1 and j <= n2 -1)"
+        if(str[i] != str[j]):
+            break
+        result += (str[i])
+        i += 1
+        j += 1
+        return (result)
+def commonprefix(arr,n)
+    arr.sort(reverse=false)
+    print(commonprefixutil(arr[0],arr[n-1]))
+if__name__ == '__main__':
+    arr = [ ]
+    n=len(arr)
+    commonprefix(arr,n)
+        
