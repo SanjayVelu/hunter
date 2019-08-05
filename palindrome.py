797a117ae5819c1192e1bc580c58385646e2c75f
SanjayVelu <51081868+SanjayVelu@users.noreply.github.com>
2019-08-05 10:36:19 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -2,7 +2,7 @@ class Stack:
     def __init__(self):
         self.items = []
     def is.empty(self):
-        return self.items = []
+        return self.items == []
     def push(self, data)
         self.items.append(data)
     def pop(self,data)
