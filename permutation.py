687e9601837af7822d000a35a3309d3d7725b049
SanjayVelu <51081868+SanjayVelu@users.noreply.github.com>
2019-08-05 10:43:29 +0530

    Create permutation.py

---
+++ permutation.py
@@ -0,0 +1,9 @@
+from itertools import permutations
+def allpermutations(str):
+    permList = permutations(str)
+    for perm in list(permlist):
+        print(''.join(perm))
+if__name__ == "__main__":
+   str = input("enter a value:")
+   allPermutations(str)
+    
